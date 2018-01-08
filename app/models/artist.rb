717c13b0c137ded7b244e19f6dd0af9523063406
Yomaira Escano <yjescano@gmail.com>
2018-01-08 03:58:26 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    self.genres
+    self.genres.first
     # binding.pry
   end
 

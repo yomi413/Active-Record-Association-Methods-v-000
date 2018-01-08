13ee5554db1712386e285e044d579937555a0e0b
Yomaira Escano <yjescano@gmail.com>
2018-01-08 04:06:49 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.name
+    # self.artists.
+    binding.pry
   end
 end
